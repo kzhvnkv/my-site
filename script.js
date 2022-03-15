e01034b325368b392ebc407a72029ad3e2e8fe93
kzhvnkv <75725142+kzhvnkv@users.noreply.github.com>
2022-03-15 13:34:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let page = document.querySelector('.page');
-//page.classList.add('dark-theme');
-let themeButton = document.querySelector('.theme-button')
-themeButton.onclick = function () {
-    page.classList.toggle('dark-theme');
-}
